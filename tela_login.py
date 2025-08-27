b4df412a1eb3a3bed7a7da4c2ecc2cb2829cfa97
Felipe Gomes <felipegomes0253@gmail.com>
2025-08-27 18:49:02 -0300

    atu

---
+++ tela_login.py
@@ -0,0 +1,28 @@
+import customtkinter as ctk 
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+#janela de login
+janela = ctk.CTk()
+janela.geometry('1500x900')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+janela.mainloop()
